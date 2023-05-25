c4877926d94612f692fb6658765a0e6745c82424
Yongjin Kim <lepetitprinz0911@gmail.com>
2023-05-26 01:18:35 +0900

    create a dag file applying no catchup

---
+++ processing-user-event/dags/no_catchup.py
@@ -0,0 +1,48 @@
+import datetime as dt
+from pathlib import Path
+
+import pandas as pd
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+dag = DAG(
+    dag_id="no_catchup",
+    schedule_interval="@daily",
+    start_date=dt.datetime(2023, 1, 1),
+    catchup=False
+)
+
+fetch_events = BashOperator(
+    task_id="fetch_events",
+    bash_command=(
+        "mkdir -p /data/events && "
+        "curl -o /data/events/{{ds}}.json "
+        "http://events_api:5000/events?"
+        "start_date={{ds}}&"
+        "end_date={{next_ds}}"
+    ),
+    dag=dag
+)
+
+def _calculae_stats(**context):
+    input_path = context["templates_dict"]["input_path"]
+    output_path = context["template_dict"]["output_path"]
+
+    events = pd.read_json(input_path)
+    stats = events.groupby(["date", "user"]).size().reset_index()
+
+    Path(output_path).parent.mkdir(exist_ok=True)
+    stats.to_csv(output_path, index=False)
+
+calculate_stats = PythonOperator(
+    task_id="calculate_stats",
+    python_callable=_calculae_stats,
+    templates_dict={
+        "input_path": "/data/events/{{ds}}.json",
+        "output_path": "/data/stats/{{ds}}.csv",
+    },
+    dag=dag
+)
+
+fetch_events >> calculate_stats
